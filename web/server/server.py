04208d8366ca65aa6be4d363c0e8ec9a19d2c2f7
Revya <revya25@gmail.com>
2024-01-14 09:00:51 +0800

    Fixed cors problem

--- web/server/server.py
+++ web/server/server.py
@@ -1,8 +1,9 @@
 
-
+from flask_cors import CORS
 from api import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
+  CORS(app, origins=["http://localhost:5173"])
   app.run(debug = True)
