370b4032c9760e11f8c2ee14acae1fe65c3605b1
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-01-22 14:14:36 +0800

    fixed CORS error

--- web/server/server.py
+++ web/server/server.py
@@ -14,9 +14,21 @@
 # if __name__ == "__main__":
 #     app.run(debug=True)
 
+from flask import Flask, jsonify
 from api import create_app
+from flask_cors import CORS
 
-app = create_app()
+# app = create_app()
+app = Flask(__name__)
+CORS(app, origins=["http://localhost:5173"])
+
+
+@app.route("/api/home", methods=['GET'])
+def return_home():
+     return jsonify({
+    'message': 'Hello World',
+    'animal': ['goat', 'lion', 'chicken']
+  })
 
 if __name__ == '__main__':
   app.run(debug = True)
