cf83cd25a5c32c06feeee8c56d373e45d5313065
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-01-23 14:55:57 +0800

    created UserResource and added GET function

--- web/server/api/user_api.py
+++ web/server/api/user_api.py
@@ -1,29 +1,23 @@
-from flask import Blueprint, request,  jsonify
+from flask_restful import Resource
 from firebase_admin import firestore
 import uuid
 
 db = firestore.client()
 user_ref = db.collection('user')
 
-user_api = Blueprint('user_api', __name__)
-
-@user_api.route('/add', methods=['POST'])
-def create():
-  try:
-    id = uuid.uuid4()
-    
-    user_ref.document(id.hex).set(request.json)
-    
-    return jsonify({"success" : True}), 200
-  except Exception as e:
-    return f"An error Occured: {e}"
-  
-  
-@user_api.route('/list', methods=['GET'])
-def read():
-  try:
-    all_users = [doc.to_dict() for doc in user_ref.stream()]
-    
-    return jsonify(all_users), 200
-  except Exception as e:
-    return f"An Error Occured: {e}"
+class UserResource(Resource):
+  def get(self, user_id=None):
+    try:
+      if user_id:
+        #! fetching a single user
+        user = user_ref.document(user_id).get()
+        if user.exists:
+          return { "data": user.to_dict() }, 200
+        else:
+          return {"error": "User not found"}, 404
+      else:
+        all_users = [doc.to_dict() for doc in user_ref.stream()]
+      
+        return { "data": all_users }, 200
+    except Exception as e:
+      return f"An Error Occured: {e}"
--- web/server/server.py
+++ web/server/server.py
@@ -1,28 +1,8 @@
-
-from flask import jsonify
 from flask_cors import CORS
-from flask_restful import Api, Resource
 from api import create_app
 
 app = create_app()
-# app = Flask(__name__)
-api = Api(app)
 CORS(app, origins=["http://localhost:5173"])
-
-
-@app.route("/api/home", methods=['GET'])
-def return_home():
-     return jsonify({
-    'message': 'Hello World',
-    'animal': ['goat', 'lion', 'chicken']
-  })
-
-class HelloWorld(Resource):
-    def get(self):
-        return { "message": "Hello world" }
-
-
-api.add_resource(HelloWorld, "/")
 
 if __name__ == '__main__':
   app.run(debug = True)
