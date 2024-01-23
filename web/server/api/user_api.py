651fd7fdc79ed634e11672751f738d848be0cec1
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-01-23 15:08:39 +0800

    can now fetch doc_id

--- web/server/api/user_api.py
+++ web/server/api/user_api.py
@@ -12,12 +12,13 @@ class UserResource(Resource):
         #! fetching a single user
         user = user_ref.document(user_id).get()
         if user.exists:
-          return { "data": user.to_dict() }, 200
+          return { "data": user.to_dict(), "doc_id": user.id }, 200
         else:
           return {"error": "User not found"}, 404
       else:
-        all_users = [doc.to_dict() for doc in user_ref.stream()]
-      
-        return { "data": all_users }, 200
+        all_users = [{"doc_id": doc.id, **doc.to_dict()} for doc in user_ref.stream()]
+        return {"data": all_users}, 200
     except Exception as e:
       return f"An Error Occured: {e}"
+    
+  # def post(self, )
