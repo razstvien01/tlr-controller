5609d8ed250b1d974053524771d767a852d8005b
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-01-23 15:47:23 +0800

    added put function in UserResource class

--- web/server/api/user_api.py
+++ web/server/api/user_api.py
@@ -6,6 +6,16 @@ import uuid
 db = firestore.client()
 user_ref = db.collection('users')
 
+def parse_user_args():
+  parser = reqparse.RequestParser()
+  
+  parser.add_argument('name', type=str, required=True, help='Name cannot be blank')
+  parser.add_argument('age', type=int, required=True, help='Age cannot be blank')
+  parser.add_argument('address', type=str, required=True, help='Address cannot be blank')
+  parser.add_argument('email', type=str, required=True, help='Email cannot be blank')
+  
+  return parser.parse_args()
+
 class UserResource(Resource):
   def get(self, user_id=None):
     try:
@@ -24,13 +34,7 @@ class UserResource(Resource):
     
   def post(self):
     try:
-      parser = reqparse.RequestParser()
-      parser.add_argument('name', type=str, required=True, help='Name cannot be blank')
-      parser.add_argument('age', type=int, required=True, help='Age cannot be blank')
-      parser.add_argument('address', type=str, required=True, help='Address cannot be blank')
-      parser.add_argument('email', type=str, required=True, help='Email cannot be blank')
-      
-      args = parser.parse_args()
+      args = parse_user_args()
       
       # ! Check if the email already exist
       existing_user = user_ref.where('email', '==', args['email']).stream()
@@ -47,7 +51,29 @@ class UserResource(Resource):
       
       user_ref.add(new_user_data)
       
-      return jsonify(Success=True)
+      return jsonify(Success=True).get_json(), 200
+      
+    except Exception as e:
+      return f"An Error Occured: {e}"
+    
+  def put(self, user_id):
+    try:
+      args = parse_user_args()
+      
+      user = user_ref.document(user_id).get()
+      
+      if not user.exists:
+        return jsonify({"error", "User not found"}).get_json(), 404
+      
+      #* Updare all the data of the existing user
+      user_ref.document(user_id).update({
+        'name': args['name'],
+        'age': args['age'],
+        'address': args['address'],
+        'email': args['email'],
+      })
+      
+      return jsonify(Success=True).get_json(), 200
       
     except Exception as e:
       return f"An Error Occured: {e}"
