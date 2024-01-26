70a5c77196a825119b5b8997a22c8c148c64772e
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-01-26 17:02:03 +0800

    updated server and api init folder

--- web/server/api/__init__.py
+++ web/server/api/__init__.py
@@ -1,25 +1,31 @@
-
 from flask import Flask
 from flask_restful import Api
 from firebase_admin import credentials, initialize_app
+from flask_socketio import SocketIO
+from flask_cors import CORS  # Import CORS
 
 cred = credentials.Certificate("key.json")
 default_app = initialize_app(cred)
 
 def create_app():
-  app = Flask(__name__)
-  api = Api(app)
-  app.config['SECRET_KEY'] = 'qwerty12345'
-  
-  from .control_session import control_api
-  from .user_api import UserResource
-  from .robot_api import RobotResource
-  
-  # ! CONTROLLER API
-  app.register_blueprint(control_api, url_prefix='/controller')
-  
-  # ! REST APIs
-  api.add_resource(UserResource, "/users", "/users/<string:user_id>")
-  api.add_resource(RobotResource, "/robots", "/robots/<string:robot_id>")
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'qwerty12345'
+    api = Api(app)
 
-  return app
+    # Use CORS middleware
+    CORS(app)
+    socketio = SocketIO(app, cors_allowed_origins="*")
+    socketio.init_app(app)  # Initialize SocketIO
+
+    from .control_session import control_api
+    from .user_api import UserResource
+    from .robot_api import RobotResource
+
+    # ! CONTROLLER API
+    app.register_blueprint(control_api, url_prefix='/controller')
+
+    # ! REST APIs
+    api.add_resource(UserResource, "/users", "/users/<string:user_id>")
+    api.add_resource(RobotResource, "/robots", "/robots/<string:robot_id>")
+
+    return app, socketio  # Return both app and socketio instances
--- web/server/server.py
+++ web/server/server.py
@@ -1,20 +1,19 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
-from flask_cors import CORS
-from api import create_app
+# server.py
 
-app = create_app()
-CORS(app, origins=["*"])
-socketio = SocketIO(app, cors_allowed_origins="*")
+from flask import render_template
+from api import create_app
+from flask_socketio import SocketIO
+
+app, socketio = create_app()  # Use the create_app function
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "WELCOME TO THE SERVER"
 
 @socketio.on('message_from_client')
 def handle_message(message):
     print('Received message:', message)
     socketio.emit('response_to_client', {'data': 'Message received successfully from the server!'})
-
+    
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True)  # Use socketio.run to run the server
