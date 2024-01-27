d8a017c743d4830c90a23399b6e718c15ed09ed5
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-01-27 14:00:41 +0800

    updated server url and server.py file

--- web/server/server.py
+++ web/server/server.py
@@ -13,5 +13,5 @@ def handle_message(message):
     print('Received message:', message)
     socketio.emit('response_to_client', {'data': 'Message received successfully from the server!'})
     
-# if __name__ == '__main__':
-#     socketio.run(app, debug=True)  # Use socketio.run to run the server
+if __name__ == '__main__':
+    socketio.run(app, debug=True)  # Use socketio.run to run the server
