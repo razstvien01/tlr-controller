c2736cf881f9cbc013059cb9b28bbe0e2a1ae188
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-02-04 21:53:21 +0800

    modified server.py and api init file. Found the cause of api error

--- web/server/server.py
+++ web/server/server.py
@@ -1,21 +1,33 @@
-from api import create_app
+from flask import Flask, render_template
+from flask_restful import Api
+from flask_socketio import SocketIO
+from flask_cors import CORS
+from api.user_api import UserResource
+from api.robot_api import RobotResource
+from sockets.socket_functions import setup_socket_io
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-app, socketio = create_app()
-
 debug_mode = os.getenv('FLASK_DEBUG', False)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'qwerty12345'
+api = Api(app)
+CORS(app)
+
+api.add_resource(UserResource, "/users", "/users/<string:user_id>")
+api.add_resource(RobotResource, "/robots", "/robots/<string:robot_id>")
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# Register socket events
+setup_socket_io(socketio)
 
 @app.route('/')
 def index():
-    return "WELCOME TO THE SERVER"
+    return render_template("index.html")
 
-@socketio.on('message_from_client')
-def handle_message(message):
-    print('Received message:', message)
-    socketio.emit('response_to_client', {'data': 'Message received successfully from the server!'})
-    
 if __name__ == '__main__':
-    socketio.run(app, debug=debug_mode)  #! Use socketio.run to run the server
+    socketio.run(app, debug=debug_mode)
