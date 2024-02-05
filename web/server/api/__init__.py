0661502af9bd7baf5a19fe23a889f639e3bb4d04
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-02-05 16:25:35 +0800

    modified __init__.py from api folder

--- web/server/api/__init__.py
+++ web/server/api/__init__.py
@@ -1,4 +1,6 @@
 from firebase_admin import credentials, initialize_app
+import os
 
-cred = credentials.Certificate("key.json")
+key_path = os.environ.get("FIREBASE_KEY_PATH", "key.json")
+cred = credentials.Certificate(key_path)
 default_app = initialize_app(cred)
