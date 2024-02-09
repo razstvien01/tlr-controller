50903e8a53c2d37ff53c14df70c73786c3611a7f
Revya <revya25@gmail.com>
2024-02-09 10:41:33 +0800

    Added Ctrl+C Keyboard Interrupt

--- web/server/api/__init__.py
+++ web/server/api/__init__.py
@@ -1,31 +1,6 @@
 from firebase_admin import credentials, initialize_app
 import os
 
-cred = credentials.Certificate("key.json")
-default_app = initialize_app(cred)
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'qwerty12345'
-    api = Api(app)
-
-    # Use CORS middleware
-    CORS(app)
-    socketio = SocketIO(app, cors_allowed_origins="*")
-    socketio.init_app(app)  # Initialize SocketIO
-
-    from .control_session import configure_controller_sockets
-    from .user_api import UserResource
-    from .robot_api import RobotResource
-
-    # ! CONTROLLER API
-    configure_controller_sockets(socketio)
-
-    # ! REST APIs
-    api.add_resource(UserResource, "/users", "/users/<string:user_id>")
-    api.add_resource(RobotResource, "/robots", "/robots/<string:robot_id>")
-
-    return app, socketio  # Return both app and socketio instances
 key_path = os.environ.get("FIREBASE_KEY_PATH", "key.json")
 cred = credentials.Certificate(key_path)
 default_app = initialize_app(cred)
--- web/server/server.py
+++ web/server/server.py
@@ -4,9 +4,11 @@ from flask_socketio import SocketIO
 from flask_cors import CORS
 from api.user_api import UserResource
 from api.robot_api import RobotResource
+from api.control_session import configure_controller_sockets
 from sockets.socket_functions import setup_socket_io
 from dotenv import load_dotenv
 import os
+import signal
 
 import grpc.experimental.gevent as grpc_gevent
 
@@ -27,6 +29,8 @@ api.add_resource(RobotResource, "/api/robots", "/api/robots/<string:robot_id>")
 
 socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins="*")
 
+configure_controller_sockets(socketio)
+
 # Hello World API endpoint
 class HelloWorld(Resource):
     def get(self):
@@ -41,7 +45,12 @@ setup_socket_io(socketio)
 def index():
     return render_template("index.html")
 
+def stop_server(signal, frame):
+    socketio.stop()
 
 if __name__ == '__main__':
     print('running server at 5000')
+    signal.signal(signal.SIGINT, stop_server)
     socketio.run(app, debug=debug_mode)  #! Use socketio.run to run the server
+
+
