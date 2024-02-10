a2bfd679b1e87aa4799233e638c7af7b0c6a5e26
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-02-10 16:32:14 +0800

    added getUsers functions in the users.controller.ts

--- web/client/app/api/users/route.ts
+++ web/client/app/api/users/route.ts
@@ -1,14 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { USER_QUERY } from '../../../types/constants'
+import { getUsers } from '../../../controllers/users.controller'
 
 
 export const GET = async (request: NextRequest, response: NextResponse) => {
   const url = new URL(request.url)
   const query = url.searchParams.get("query")
   
-  
-  return NextResponse.json({
-    success: true,
-    message: "Users Fetch Successfully"
-  })
+  return getUsers()
 }
