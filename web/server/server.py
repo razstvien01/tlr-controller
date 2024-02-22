6e9527c9a17b7eb581ad8136e402d67fdf97caf8
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-02-22 23:10:13 +0800

    cleaned up server.py and deleted socket module

--- web/server/server.py
+++ web/server/server.py
@@ -5,14 +5,9 @@ from flask_cors import CORS
 from api.user_api import UserResource
 from api.robot_api import RobotResource
 from api.control_session import configure_controller_sockets
-from sockets.socket_functions import setup_socket_io
 from dotenv import load_dotenv
 import os
 import signal
-
-import grpc.experimental.gevent as grpc_gevent
-
-grpc_gevent.init_gevent()
 
 load_dotenv()
 
@@ -24,22 +19,9 @@ app.config['SECRET_KEY'] = secret_key
 api = Api(app)
 CORS(app)
 
-api.add_resource(UserResource, "/api/users", "/api/users/<string:user_id>")
-api.add_resource(RobotResource, "/api/robots", "/api/robots/<string:robot_id>")
-
 socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins="*")
 
 configure_controller_sockets(socketio)
-
-# Hello World API endpoint
-class HelloWorld(Resource):
-    def get(self):
-        return {'message': 'Hello, World!'}
-
-api.add_resource(HelloWorld, '/api/hello')
-
-# Register socket events
-setup_socket_io(socketio)
 
 @app.route('/')
 def index():
@@ -52,5 +34,3 @@ if __name__ == '__main__':
     print('running server at 5000')
     signal.signal(signal.SIGINT, stop_server)
     socketio.run(app, debug=debug_mode)  #! Use socketio.run to run the server
-
-
