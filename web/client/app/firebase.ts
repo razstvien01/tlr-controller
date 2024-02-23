91e15a1f7d27c6208606d1a28331420ac22e7c1e
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-02-23 10:36:16 +0800

    hide firebase sensitive data

--- web/client/app/firebase.ts
+++ web/client/app/firebase.ts
@@ -8,12 +8,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDAQUgmmHVOWOQvo6IKH3LgNjVFK79yjuo",
-  authDomain: "tlr-controller.firebaseapp.com",
-  projectId: "tlr-controller",
-  storageBucket: "tlr-controller.appspot.com",
-  messagingSenderId: "268288052952",
-  appId: "1:268288052952:web:9d8fa6436cdc2185fa11ad"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
