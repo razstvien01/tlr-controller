6fa1712de3e3f71ae8e050625c1403c15d6d7fcc
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-02-24 13:35:41 +0800

    added some UI colors based on the colors that are defined in the global.css

--- web/client/app/page.tsx
+++ web/client/app/page.tsx
@@ -1,24 +1,22 @@
 // pages/index.js
 import ControllerTest from "@/components/test/controller_test";
 import Link from "next/link";
-import { ModeToggle } from "@/components/mode-toggle"
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { Header } from "@/components/header"
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       {/* Header */}
-      <header className="bg-blue-500 p-4 text-white">
-        <h1 className="text-2xl font-bold">Telepresence Robot App</h1>
-        <ModeToggle/>
-        {/* Add any additional header content */}
-      </header>
+      <Header/>
 
       {/* Main Content with Background Image */}
       <main
         className="flex-1 flex flex-col items-center justify-center bg-cover bg-center"
         style={{ backgroundImage: "url('bg-1.gif')" }}
       >
-        <div className="text-center p-8 rounded-lg opacity-80">
+        <div className="text-center bg-background text-foreground p-8 rounded-lg opacity-80">
           <h2 className="text-3xl font-semibold mb-4">
             Welcome to Telepresence Robot Control Center
           </h2>
@@ -27,15 +25,15 @@ export default function Home() {
             Control your robot seamlessly from anywhere in the world.
           </p>
           <Link href="/login">
-            <div className="bg-blue-500 text-white px-6 py-3 rounded-full text-lg font-semibold transition hover:bg-blue-600">
+            <Button className="px-6 py-3 rounded-full text-lg font-semibold transition ">
               Get Started
-            </div>
+            </Button>
           </Link>
         </div>
       </main>
 
       {/* Footer */}
-      <footer className="bg-blue-500 p-4 text-white">
+      <footer className="bg-primary p-4 text-foreground">
         <p>&copy; 2024 Telepresence Robot App</p>
         {/* Add any additional footer content */}
       </footer>
