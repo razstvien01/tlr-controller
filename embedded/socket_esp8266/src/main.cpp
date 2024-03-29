53e3585f99f32c8d34a142fe17a1b5813eadd2e7
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-03-29 22:45:36 +0800

    cleaned up

--- embedded/socket_esp8266/src/main.cpp
+++ embedded/socket_esp8266/src/main.cpp
@@ -6,22 +6,6 @@
 #include "socketio_client.h"
 
 WifiHelper wifiHelper;
-// WebSocketClient webSocketClient;
-
-// void setup()
-// {
-//   Serial.begin(9600);
-//   delay(1000);
-
-//   wifiHelper.wifiConnect(WIFI_SSID, WIFI_PASSWORD);
-//   // webSocketClient.begin(TEST_WEBSOCKETS_HOST, TEST_WEBSOCKETS_PORT);
-//   webSocketClient.begin(TEST_WEBSOCKETS_HOST, TEST_WEBSOCKETS_PORT);
-// }
-
-// void loop()
-// {
-//   webSocketClient.loop();
-// }
 
 SocketIOClient socketIOClient;
 
@@ -30,15 +14,15 @@ void setup()
   Serial.begin(9600);
   delay(1000);
 
-  // Connect to WiFi
+  //! Connect to WiFi
   wifiHelper.wifiConnect(WIFI_SSID, WIFI_PASSWORD);
 
-  // Initialize SocketIOClient
+  //! Initialize SocketIOClient
   socketIOClient.begin(SOCKETIO_HOST, SOCKETIO_PORT);
 }
 
 void loop()
 {
   socketIOClient.loop();
-  delay(1000); // Adjust the delay as needed
+  delay(1000);
 }
