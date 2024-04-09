055025b4421331379cc8c82a2e4d487fe7fe193d
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-04-09 14:40:45 +0800

    moved socketio client related functions to the socketio_manager header

---
+++ embedded/socket_esp8266/lib/socketio_manager/socketio_manager.cpp
@@ -0,0 +1,88 @@
+#include "socketio_manager.h"
+
+SocketIOManager::SocketIOManager() {
+    
+}
+
+void SocketIOManager::onEvent(socketIOmessageType_t type, uint8_t *payload, size_t length) {
+  switch (type)
+  {
+  case sIOtype_DISCONNECT:
+    Serial.printf("[IOc] Disconnected!\n");
+    break;
+  case sIOtype_CONNECT:
+    Serial.printf("[IOc] Connected to url: %s\n", payload);
+
+    // join default namespace (no auto join in Socket.IO V3)
+    socketIO.send(sIOtype_CONNECT, "/");
+    break;
+  case sIOtype_EVENT:
+  {
+    char *sptr = NULL;
+    int id = strtol((char *)payload, &sptr, 10);
+    Serial.printf("[IOc] get event: %s id: %d\n", payload, id);
+    if (id)
+    {
+      payload = (uint8_t *)sptr;
+    }
+    DynamicJsonDocument doc(1024);
+
+    DeserializationError error = deserializeJson(doc, payload, length);
+
+    if (error)
+    {
+      Serial.print(F("deSerializeJson() failed: "));
+      Serial.println(error.c_str());
+      return;
+    }
+
+    String eventName = doc[0];
+    Serial.printf("[IOc] event name: %s\n", eventName.c_str());
+
+    // Message Includes a ID for a ACK (callback)
+    if (id)
+    {
+      // creat JSON message for Socket.IO (ack)
+      DynamicJsonDocument docOut(1024);
+      JsonArray array = docOut.to<JsonArray>();
+
+      // add payload (parameters) for the ack (callback function)
+      JsonObject param1 = array.createNestedObject();
+      param1["now"] = millis();
+
+      // JSON to String (Serializion)
+      String output;
+      output += id;
+
+      serializeJson(docOut, output);
+
+      // Send event
+      socketIO.send(sIOtype_ACK, output);
+    }
+  }
+  break;
+  case sIOtype_ACK:
+    Serial.printf("[IOc] get ack: %u\n", length);
+    break;
+  case sIOtype_ERROR:
+    Serial.printf("[IOc] get error: %u\n", length);
+    break;
+  case sIOtype_BINARY_EVENT:
+    Serial.printf("[IOc] get binary: %u\n", length);
+    break;
+  case sIOtype_BINARY_ACK:
+    Serial.printf("[IOc] get binary ack: %u\n", length);
+    break;
+  }
+}
+
+void SocketIOManager::begin(const char* host, uint16_t port, const char* path) {
+    socketIO.begin(host, port, path);
+    socketIO.onEvent([this](socketIOmessageType_t type, uint8_t *payload, size_t length) {
+        this->onEvent(type, payload, length);
+    });
+}
+
+void SocketIOManager::loop() {
+  socketIO.loop();
+}
---
+++ embedded/socket_esp8266/lib/socketio_manager/socketio_manager.h
@@ -0,0 +1,19 @@
+#ifndef SOCKETIO_MANAGER_H
+#define SOCKETIO_MANAGER_H
+
+#include <Arduino.h>
+#include <SocketIOclient.h>
+#include <ArduinoJson.h>
+
+class SocketIOManager {
+public:
+    SocketIOManager();
+    void begin(const char* host, uint16_t port, const char* path);
+    void loop();
+    void onEvent(socketIOmessageType_t type, uint8_t *payload, size_t length);
+
+private:
+    SocketIOclient socketIO;
+};
+
+#endif
