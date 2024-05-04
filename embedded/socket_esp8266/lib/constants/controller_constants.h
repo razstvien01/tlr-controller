f74bed0bf304aa3d9936d6004c547939d5fd90b2
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-05-04 09:59:09 +0800

    added constants socket routes for the requests and responses

--- embedded/socket_esp8266/lib/constants/controller_constants.h
+++ embedded/socket_esp8266/lib/constants/controller_constants.h
@@ -1,1 +1,13 @@
-#define C_RES_TURNONROBOT "controller/TurnOnRobot/response"
+//* Request
+#define C_REQ_TURNON_ROBOT "controller/TurnOnRobot/request"
+#define C_REQ_TURNOFF_ROBOT "controller/TurnOffRobot/request"
+#define C_REQ_USE_ROBOT "controller/UseRobot/request"
+#define C_REQ_CONTROL_ROBOT "controller/ControlRobot/request"
+#define C_REQ_GET_CONTROL "controller/GetControl/request"
+
+//* Responses
+#define C_RES_TURNON_ROBOT "controller/TurnOnRobot/response"
+#define C_RES_TURNOFF_ROBOT "controller/TurnOffRobot/response"
+#define C_RES_USE_ROBOT "controller/UseRobot/response"
+#define C_RES_CONTROL_ROBOT "controller/ControlRobot/response"
+#define C_RES_GET_CONTROL "controller/GetControl/response"
