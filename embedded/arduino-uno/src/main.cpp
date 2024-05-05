d8288334eccd0c1d0a1928dfa05f81f6c8174ebc
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-05-05 12:30:40 +0800

    send json data to the arduino, and receive json data from arduino

--- embedded/arduino-uno/src/main.cpp
+++ embedded/arduino-uno/src/main.cpp
@@ -1,16 +1,47 @@
-#include <Arduino.h>
+#include <ArduinoJson.h>
 
-void setup(){
+StaticJsonDocument<200> doc;
+
+void setup()
+{
+
   Serial.begin(9600);
+  delay(1000);
 }
 
-void loop() {
-  Serial.println("Hello from Arduino");
-  
-  if(Serial.available()){
-    String data = Serial.readStringUntil('\n');
-    Serial.print("Received from esp8266: ");
-    Serial.println(data);
+void sendDataToESP()
+{
+  // Create and populate the JSON data
+  JsonObject data = doc.to<JsonObject>();
+  data["key"] = "value";      // Add your JSON data here
+  data["source"] = "arduino"; // Add the source information
+  serializeJson(data, Serial);
+  Serial.println(); // Add a newline to indicate the end of JSON data
+}
+
+void loop()
+{
+  sendDataToESP(); // Send JSON data to the ESP every 5 seconds
+
+  if (Serial.available())
+  {
+    // Process incoming data from the ESP
+    deserializeJson(doc, Serial);
+    const char *key = doc["key"];
+    if (key)
+    {
+      // Print the received key
+      Serial.print("Received key: ");
+      Serial.println(key);
+
+      // Check if the data is received from the ESP
+      const char *source = doc["source"];
+      if (source && strcmp(source, "esp") == 0)
+      {
+        Serial.println("Data received from ESP8266");
+      }
+    }
   }
-  delay(5000);
+
+  delay(1000); // Delay for 5 seconds before sending the next JSON data
 }
--- embedded/socket_esp8266/src/main.cpp
+++ embedded/socket_esp8266/src/main.cpp
@@ -8,32 +8,60 @@
 WifiHelper wifiHelper;
 SocketIOManager socketIOManager;
 
-
 void setup()
 {
   Serial.begin(9600);
-  // delay(1000);
+  delay(1000);
 
   // //! Connect to WiFi
-  // wifiHelper.wifiConnect(WIFI_SSID, WIFI_PASSWORD);
+  wifiHelper.wifiConnect(WIFI_SSID, WIFI_PASSWORD);
 
-  // //! Initialize SocketIOClient
-  // socketIOManager.begin(SOCKETIO_LOCALHOST, SOCKETIO_LOCALPORT, SOCKETIO_URL);
+  //! Initialize SocketIOClient
+  socketIOManager.begin(SOCKETIO_LOCALHOST, SOCKETIO_LOCALPORT, SOCKETIO_URL);
+}
+
+StaticJsonDocument<200> doc; // Adjust the size as needed
+
+void sendDataToArduino()
+{
+  // Create and populate the JSON data
+  JsonObject data = doc.to<JsonObject>();
+  data["key"] = "value";  // Add your JSON data here
+  data["source"] = "esp"; // Add the source information
+  serializeJson(data, Serial);
+  Serial.println(); // Add a newline to indicate the end of JSON data
 }
 
 void loop()
 {
-  // socketIOManager.loop();
+  socketIOManager.loop();
 
-  // if (!wifiHelper.isConnected())
-  // {
-  //   wifiHelper.wifiConnect(WIFI_SSID, WIFI_PASSWORD);
-  // }
-  Serial.println("Hello from 8266");
-  if(Serial.available()){
-    String data = Serial.readStringUntil('\n');
-    Serial.print("receive from arduino: ");
-    Serial.println(data);
+  if (!wifiHelper.isConnected())
+  {
+    wifiHelper.wifiConnect(WIFI_SSID, WIFI_PASSWORD);
   }
-  delay(5000);
+
+  // sendDataToArduino(); // Send JSON data to the ESP every 5 seconds
+
+  // if (Serial.available())
+  // {
+  //   // Process incoming data from the ESP
+  //   deserializeJson(doc, Serial);
+  //   const char *key = doc["key"];
+  //   if (key)
+  //   {
+  //     // Print the received key
+  //     Serial.print("Received key: ");
+  //     Serial.println(key);
+
+  //     // Check if the data is received from the ESP
+  //     const char *source = doc["source"];
+  //     if (source && strcmp(source, "arduino") == 0)
+  //     {
+  //       Serial.println("Data received from Aruino");
+  //     }
+  //   }
+  // }
+
+  // delay(1000); 
 }
