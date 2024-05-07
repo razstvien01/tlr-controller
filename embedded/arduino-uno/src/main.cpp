dd0474c8023d4baff26d16d2e3540c6ca77cf36a
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-05-07 12:46:28 +0800

    added handleRecievedData() and sendDataToESP(), refactor code

--- embedded/arduino-uno/src/main.cpp
+++ embedded/arduino-uno/src/main.cpp
@@ -6,59 +6,58 @@ StaticJsonDocument<200> doc;
 
 void setup()
 {
-
   Serial.begin(9600);
   delay(1000);
+}
+
+void handleControlEvent(JsonObject payload) {
+  if (payload.containsKey("statusCode")) {
+    int statusCode = payload["statusCode"];
+    Serial.print("Status Code: ");
+    Serial.println(statusCode);
+  } else {
+    int drive = payload["Drive"];
+    int steer = payload["Steer"];
+    
+    Serial.print("Drive: ");
+    Serial.print(drive);
+    Serial.print(", Steer: ");
+    Serial.println(steer);
+  }
+}
+
+void handleReceivedData() {
+  String receivedData = Serial.readStringUntil('\n');
+  StaticJsonDocument<200> receivedDoc;
+  DeserializationError error = deserializeJson(receivedDoc, receivedData);
+
+  if (!error) {
+    const char *event = receivedDoc[0];
+    JsonObject payload = receivedDoc[1];
+
+    if (strcmp(event, C_RES_TURNOFF_ROBOT) == 0) {
+      // Handle turn off robot event
+    } else if (strcmp(event, C_RES_GET_CONTROL) == 0) {
+      handleControlEvent(payload);
+    }
+  } else {
+    Serial.println("JSON Parsing Error");
+  }
 }
 
 void sendDataToESP()
 {
   JsonObject data = doc.to<JsonObject>();
-  data["key"] = "value";
-  data["source"] = "arduino";
+  data["key"] = "Sensor Info";
+  data["message"] = "BLAH BLAH BLAH BLAH";
   serializeJson(data, Serial);
   Serial.println();
 }
 
 void loop()
 {
+  sendDataToESP();
   if (Serial.available()) {
-    String receivedData = Serial.readStringUntil('\n');
-    // Serial.println("Received JSON: " + receivedData);
-
-    StaticJsonDocument<200> receivedDoc;
-    DeserializationError error = deserializeJson(receivedDoc, receivedData);
-    
-    if (!error) {
-      //* Extract the event and payload
-      const char *event = receivedDoc[0];
-      JsonObject payload = receivedDoc[1];
-      
-      // Serial.print("Event: ");
-      // Serial.println(event);
-      
-      if(strcmp(event, C_RES_TURNOFF_ROBOT) == 0){
-        
-      }
-      else if (strcmp(event, C_RES_GET_CONTROL) == 0) {
-        // Serial.println(receivedData);
-        if (payload.containsKey("statusCode")) {
-          int statusCode = payload["statusCode"];
-          Serial.print("Status Code: ");
-          Serial.println(statusCode);
-        } else {
-          int drive = payload["Drive"];
-          int steer = payload["Steer"];
-          
-          Serial.print("Drive: ");
-          Serial.print(drive);
-          Serial.print(", Steer: ");
-          Serial.println(steer);
-        }
-      }
-      
-    } else {
-      Serial.println("JSON Parsing Error");
-    }
+    handleReceivedData();
   }
 }
