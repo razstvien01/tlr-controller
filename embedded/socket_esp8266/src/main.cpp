991019b9caafa25510e4814dfed8aea17bd997c7
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-05-09 09:45:42 +0800

    implemented code where you try to ping the server

--- embedded/socket_esp8266/src/main.cpp
+++ embedded/socket_esp8266/src/main.cpp
@@ -1,6 +1,7 @@
 #include "secrets.h"
 
 #include <Arduino.h>
+#include <ESP8266Ping.h>
 
 #include "wifi_helper.h"
 #include "socketio_manager.h"
@@ -12,9 +13,16 @@ void setup()
 {
   Serial.begin(9600);
   delay(1000);
+  
 
   // //! Connect to WiFi
   wifiHelper.wifiConnect(WIFI_SSID, WIFI_PASSWORD);
+  
+  if(Ping.ping(SOCKETIO_HOST)){
+    Serial.println("Can now ping the socketio host");
+  }  else{
+    Serial.println("Can't connect the server");
+  }
 
   //! Initialize SocketIOClient
   socketIOManager.begin(SOCKETIO_LOCALHOST, SOCKETIO_LOCALPORT, SOCKETIO_URL);
@@ -22,7 +30,8 @@ void setup()
 
 void loop()
 {
-  socketIOManager.loop();
+  
+  // socketIOManager.loop();
 
   if (!wifiHelper.isConnected())
   {
