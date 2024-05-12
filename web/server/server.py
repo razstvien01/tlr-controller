9a3a19cc924333fdfe70d865353cedb2d124112c
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-05-13 01:08:48 +0800

    added error handling in the db

--- web/server/server.py
+++ web/server/server.py
@@ -9,8 +9,8 @@ import firebase_admin
 
 load_dotenv()
 
-debug_mode = os.getenv('FLASK_DEBUG', False)
 secret_key = os.getenv('SECRET_KEY', '')
+key = os.getenv('FIREBASE_KEY_PATH', '')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secret_key
@@ -28,11 +28,15 @@ def index():
 def stop_server(signal, frame):
     socketio.stop()
 
-cred_obj = firebase_admin.credentials.Certificate('key.json')
-default_app = firebase_admin.initialize_app(cred_obj)
+try:
+    cred_obj = firebase_admin.credentials.Certificate(key)
+    default_app = firebase_admin.initialize_app(cred_obj)
+    print("Successfully initializing Firebase")
+except Exception as e:
+    print("Error initializing Firebase:", e)
 
 if __name__ == '__main__':
     print('running server at 5000')
     signal.signal(signal.SIGINT, stop_server)
-    socketio.run(app, port=5000, host="0.0.0.0", debug=debug_mode)
+    socketio.run(app, port=5000, host="0.0.0.0", debug=False)
     
